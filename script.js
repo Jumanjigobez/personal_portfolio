12ae92b3ab2d1b753b8fd498800cf52f6d93e5cb
Jumagobe <73429193+Jumanjigobez@users.noreply.github.com>
2022-01-13 15:14:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-
-
-var name = prompt("What is your name my Guest? ", "My Guest");
-var visitor = document.getElementById('visitor');
-var visitor2 = document.getElementById('visitor2');
-
-if (name!=null) {
-  visitor.innerHTML=name;
-  visitor2.innerHTML=name;
-}else if (name==null){
-  alert("Please provide your name :-(");
-}else{
-  alert("Error you are not a human");
-}
-
-
 $(document).ready(function(){
+
+    //loading part
+
+    function counter() {
+      var count = setInterval(function(){
+        var c = parseInt($(".counter").text());
+        $(".counter").text((++c).toString());
+
+        if(c == 100){
+          clearInterval(count);
+          $(".counter").addClass("hide");
+
+          $(".text").fadeIn(1000);
+
+          $(".text").fadeOut(1500);
+
+          $(".preloader").addClass("active");
+
+          var name = prompt("What is your name my Guest? ", "My Guest");
+
+          var visitor = document.getElementById('visitor');
+          var visitor2 = document.getElementById('visitor2');
+          var visitor3 = document.getElementById('visitor3');
+          if (name!=null) {
+            visitor.innerHTML=name;
+            visitor2.innerHTML=name;
+            visitor3.innerHTML = name;
+          }else if (name==null){
+            alert("Please provide your name :-(");
+          }else{
+            alert("Error you are not a human");
+          }
+        }
+
+
+      },60)
+    }
+
+    counter()
+
 
     $(window).scroll(function(){
 
@@ -48,6 +72,47 @@ $(document).ready(function(){
     });
 
     
+    
+
+
+
 
 
 });
+
+
+
+var mytop = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mytop.style.display = "block";
+  } else {
+    mytop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+//For social arrow appear amd dissappear:
+var dissappearArrow = document.getElementById("socialarrowDissappear");
+var appearArrow = document.getElementById("socialarrowAppear");
+
+
+function socialarrowDissappear(){
+  document.getElementById("socialarrowDissappear").style ="display:none;";
+  document.getElementById("socialarrowAppear").style = "display: inline-block;";
+}
+
+function socialarrowAppear(){
+
+  document.getElementById("socialarrowDissappear").style ="display:inline-block;";
+
+  document.getElementById("socialarrowAppear").style = "display: none;";
+}
